f91050e0dc4808175c67da073007d00f70ea110b
Matheus Bernardes <matheusbnas@gmail.com>
2025-02-17 21:02:07 -0300

    Update: Arquivos na pasta App

--- escola_manager/app/__init__.py
+++ escola_manager/app/__init__.py
@@ -3,20 +3,15 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from .models import User  # Importação corrigida
 
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
 
 
-@login.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
 def create_app():
-    app = Flask(__name__)
+    # Especifica a pasta de templates
+    app = Flask(__name__, template_folder='templates')
     app.config.from_object(Config)
 
     db.init_app(app)
@@ -24,11 +19,17 @@ def create_app():
     login.init_app(app)
     login.login_view = 'auth.login'
 
-    # Registro de Blueprints
-    from escola_manager.auth import auth as auth_blueprint
+    from .models import User
+
+    @login.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    # Registrar blueprints
+    from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-    from escola_manager.routes import admin as admin_blueprint
+    from .routes import admin as admin_blueprint
     app.register_blueprint(admin_blueprint)
 
     return app
